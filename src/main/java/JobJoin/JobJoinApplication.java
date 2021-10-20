aa0544539117709d701abb585375d080eabc5270
danisdnk <danisdnk@gmail.com>
2021-10-20 03:04:44 -0300

    se crea proyecto base con mavel y vistas estaticas

---
+++ src/main/java/JobJoin/JobJoinApplication.java
@@ -0,0 +1,13 @@
+package JobJoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobJoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobJoinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JobJoin/JobJoinApplicationTests.java
@@ -0,0 +1,13 @@
+package JobJoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobJoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
