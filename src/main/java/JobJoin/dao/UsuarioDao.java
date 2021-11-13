4302635645dfa490b6b104cd52fd2dd75ec86a86
MatiasR20 <66795464+MatiasR20@users.noreply.github.com>
2021-11-13 18:41:04 -0300

    prueba clase usuario con dao (para JPA) y controladores abm y autentificacion

---
+++ src/main/java/JobJoin/dao/UsuarioDao.java
@@ -0,0 +1,16 @@
+package JobJoin.dao;
+
+import JobJoin.models.Usuario;
+
+import java.util.List;
+
+public interface UsuarioDao {
+
+    List<Usuario> getUsuarios();
+
+    void eliminar(Long id);
+
+    void registrar(Usuario usuario);
+
+    boolean verificarCredenciales(Usuario usuario);
+}
